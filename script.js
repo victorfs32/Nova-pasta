2b035d3c90fd0e557678588a1c6e574411ae07ce
victorfs32 <jvfds66@gmail.com>
2023-04-04 23:50:37 -0300

    Update

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function scrolltop() {
-    let a = window.document;
-    a = location.href = '#home';
+  let a = window.document;
+  a = location.href = "#home";
 }
-let contact = document.getElementById('contactForm');
-contact.addEventListener('submit', function (e) {
-    e.preventDefault();
-    alert(`Pedido de desculpas não pode ser aceito devido a alguns erros técnicos`);
-})
+let contact = document.getElementById("contactForm");
+contact.addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert(
+    `Pedido de desculpas não pode ser aceito devido a alguns erros técnicos`
+  );
+});
